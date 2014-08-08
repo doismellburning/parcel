ff0236b6c6f3270338d3155d55708d0c95bea01a
Kristian Glass <git@doismellburning.co.uk>
2014-08-08 16:21:55 +0100

    Better help message

--- src/Parcel/Config.hs
+++ src/Parcel/Config.hs
@@ -3,14 +3,24 @@ module Parcel.Config (
     , defaultParcelConfig
     , fpmCommand
     , merge
+    , options
     , outputType
     , outputTypeToFPMString
+    , parseOptions
 ) where
 
-data OutputType = Deb deriving (Eq, Show)
+import System.Console.GetOpt
+
+data OutputType = Deb deriving (Enum, Eq, Show)
 
 outputTypeToFPMString :: OutputType -> String
 outputTypeToFPMString Deb = "deb"
+
+parseOutputType :: String -> OutputType
+-- This is a hack - there must be a way to generate stuff in ArgDescr etc.
+-- that produces a nicer, OptParse compatible, error :(
+parseOutputType "deb" = Deb
+parseOutputType _ = error $ "Invalid output type, must be one of: " ++ (unwords $ map outputTypeToFPMString $ enumFrom Deb)
 
 data ParcelConfig = ParcelConfig
     { fpmCommand :: Maybe FilePath
@@ -28,5 +38,25 @@ maybeMerge :: Maybe a -> Maybe a -> Maybe a
 maybeMerge x Nothing = x
 maybeMerge _ (Just x) = Just x
 
+blankParcelConfig :: ParcelConfig
+blankParcelConfig = ParcelConfig Nothing Nothing
+
 defaultParcelConfig :: ParcelConfig
-defaultParcelConfig = ParcelConfig { fpmCommand = Just "fpm", outputType = Nothing }
+defaultParcelConfig = blankParcelConfig { fpmCommand = Just "fpm" }
+
+options :: [OptDescr (ParcelConfig -> ParcelConfig)]
+options =
+    [ Option ['f'] ["fpm-command"] (ReqArg (\s c -> c { fpmCommand = Just s }) "COMMAND") "Path to the fpm command to use"
+    , Option ['t'] ["output-type"] (ReqArg (\s c -> c { outputType = Just $ parseOutputType s }) "OUTPUT_TYPE") "Output package type"
+    ]
+
+parseOptions :: [String] -> ParcelConfig
+parseOptions argv =
+    case getOpt RequireOrder options argv of
+        (o, [], []) -> foldr id blankParcelConfig o
+        (_, _, errors) -> error (concat errors ++ usageInfo header options)
+
+header :: String
+header = unlines
+    [ "Parcel: Turning code into packages - https://github.com/doismellburning/parcel" -- TODO Stop hardcoding
+    ]
