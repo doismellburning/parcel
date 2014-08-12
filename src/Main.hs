0dae95098b8d82448778205a38a166c1e917d6c1
Kristian Glass <git@doismellburning.co.uk>
2014-08-12 10:43:18 +0100

    Fixed merging order (and flipped functions for consistency)

--- src/Main.hs
+++ src/Main.hs
@@ -20,14 +20,14 @@ main =
         orgConfig = fileConfig orgConfigFile :: IO (Maybe ParcelConfig)
         packageConfig = fileConfig packageConfigFile :: IO (Maybe ParcelConfig)
 
-        c = liftA2 applyPackageConfig orgConfig (return defaultParcelConfig) :: IO ParcelConfig
-        c2 = liftA2 applyPackageConfig packageConfig c :: IO ParcelConfig
+        c = liftA2 applyPackageConfig (return defaultParcelConfig) orgConfig :: IO ParcelConfig
+        c2 = liftA2 applyPackageConfig c packageConfig :: IO ParcelConfig
         c3 = liftA2 merge c2 cliConfig
     in
         fpm =<< c3
 
-applyPackageConfig :: Maybe ParcelConfig -> ParcelConfig -> ParcelConfig
-applyPackageConfig mc = maybe id merge mc
+applyPackageConfig :: ParcelConfig -> Maybe ParcelConfig -> ParcelConfig
+applyPackageConfig c mc = maybe c (merge c) mc
 
 
 fileConfig :: FilePath -> IO (Maybe ParcelConfig)
