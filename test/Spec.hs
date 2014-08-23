9277a972753090ec41bcf6fb3376485cd8c6c40f
Kristian Glass <git@doismellburning.co.uk>
2014-08-23 19:08:36 +0100

    Auto-discover hspecs
    https://hspec.github.io/hspec-discover.html

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,1 @@
-import Test.Hspec
-
-import Parcel.Procfile
-
-sampleProcLine = "web: gunicorn 0.0.0.0:8080"
-
-main :: IO ()
-main = hspec $ do
-    describe "Parcel" $ do
-        it "correctly parses a Proc line name" $ do
-            procName (parseProc sampleProcLine) `shouldBe` "web"
-
-        it "correctly parses a Proc command" $ do
-            procCommand (parseProc sampleProcLine) `shouldBe` "gunicorn 0.0.0.0:8080"
-
-parseProc :: String -> Proc
-parseProc s = head $ parseProcfile $ s
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
